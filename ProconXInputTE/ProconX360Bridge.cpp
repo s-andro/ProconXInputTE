6a6668841e1ce853b0f79a9132d73109dfb720e0
s-andro <s-andro@users.noreply.github.com>
2024-05-17 22:41:21 +0900

    Revert "Rumble scale (#1)" (#2)
    This reverts commit e63f47dd39ba494fca3b91979403932890cee37a.

--- ProconXInputTE/ProconX360Bridge.cpp
+++ ProconXInputTE/ProconX360Bridge.cpp
@@ -99,11 +99,6 @@ namespace ProconXInputTE
 			last_input_sent_.store({timestamp, status}, std::memory_order_release);
 		});
 
-		small_rumble_parameter_.Left.MaxAmplitude *= options.RumbleScale;
-		small_rumble_parameter_.Right.MaxAmplitude *= options.RumbleScale;
-		large_rumble_parameter_.Left.MaxAmplitude *= options.RumbleScale;
-		large_rumble_parameter_.Right.MaxAmplitude *= options.RumbleScale;
-
 		rumble_thread_ = std::thread([this]
 		{
 			auto clock = std::chrono::steady_clock::now();
